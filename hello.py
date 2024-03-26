d9655ae13596710e8f41a98f6ace1f46df8cba94
Josh Cole <jcolehrt@gmail.com>
2024-03-26 13:33:24 -0400

    Dice Results Added

--- hello.py
+++ hello.py
@@ -4,4 +4,14 @@ import numpy as np
 msg = "Roll a dice!"
 print(msg)
 
-print(np.random.randint(1,20))
+result = np.random.randint(1,20)
+print(result)
+
+if result == 20:
+    print("Nat 20 baby!")
+elif result >= 11:
+    print("Good roll")
+elif result >= 2:
+    print("Ouch")
+else: 
+    print("You're screwed")
